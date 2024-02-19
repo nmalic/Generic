f34d9a14b4be78a26e25821620d71034eb53ba54
Nicoleta Malic <nmalic@student.tgm.ac.at>
2024-02-19 15:04:07 +0100

    Constructors and Attributs added

--- src/Stack.java
+++ src/Stack.java
@@ -1,2 +1,12 @@
-public class Stack {
+public class Stack<T> {
+    private Object[] array;
+    private int size;
+    private int capacity;
+    private Stack(){
+        this(10);
+    }
+    public Stack(int capacity){
+        this.capacity = capacity;
+        this.array = new Object[capacity];
+    }
 }
