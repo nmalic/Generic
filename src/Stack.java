55fbc0b61227ed48d0f5ab4106db106054c12842
Nicoleta Malic <nmalic@student.tgm.ac.at>
2024-02-19 15:06:16 +0100

    push, pop and peek added

--- src/Stack.java
+++ src/Stack.java
@@ -9,4 +9,18 @@ public class Stack<T> {
         this.capacity = capacity;
         this.array = new Object[capacity];
     }
+    public void push(T element) throws StackFullException {
+        if (size  == this.array.length) throw new StackFullException("Stack is full.");
+        this.array[size++] = element;
+    }
+
+    public T pop() throws StackEmptyException {
+        if (size == 0){ throw new StackEmptyException("Stack is empty.");}
+        T t = (T) this.array[--size];
+        return t;
+    }
+    public T peek() throws StackEmptyException {
+        if (size == 0){ throw new StackEmptyException("Stack is empty.");}
+        return (T) this.array[size -1];
+    }
 }
